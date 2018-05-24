362c75ab91d71fca70f276089f5cfa3062b3e1b4
Roberto Santos <robot-o@hotmail.com>
2018-05-24 11:26:00 -0600

    init repository

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').load()
+
+const express = require("express")
+const router = express.Router()
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+router.post("/", (request, response) => {
+    const options = {
+        amount: +request.body.amount,
+        currency: "usd",
+        description: request.body.description,
+        source: request.body.token,
+    }
+    stripe.charges.create(options, (error, charge) => {
+        error
+            ? response.status(400).json({error: error.message})
+            : response.json({data: charge});
+    })
+})
+const port = process.env.PORT || 5000
+app.use(router)
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
